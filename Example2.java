ec2f39f05ae24c3f657226cd04e7caee220bce13
zaninenocast <zaninenocast@gmail.com>
2020-05-10 01:36:01 +0530

    Second example of Arrays

---
+++ Example2.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int [] numbers = {10,20,30};
+      System.out.println(numbers);
+      System.out.println(Arrays.toString(numbers));
+	}
+
+}
