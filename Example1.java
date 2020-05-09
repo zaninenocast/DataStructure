769f9faf3e29cbe3915cb78c062237350fdecfaf
zaninenocast <zaninenocast@gmail.com>
2020-05-10 00:28:41 +0530

    Data Structure first example

---
+++ Example1.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int[] number = new int[3];
+       number[0] =10;
+       number[1] =20;
+       number[2] = 30;
+       System.out.println(number);
+       System.out.println(Arrays.toString(number));
+	}
+
+}
